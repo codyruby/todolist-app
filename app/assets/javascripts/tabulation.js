1e50d3134a43ff623f024b741ebf718685fb80f7
Yousri <laifayousri@gmail.com>
2019-06-20 17:00:58 +0200

    WIP

--- app/assets/javascripts/tabulation.js
+++ app/assets/javascripts/tabulation.js
@@ -1,3 +1,9 @@
 $(document).ready(function() {
     // Mets ici ton code
+    var $titleCard = $(".category-title");
+    $titleCard.click(function(){
+        $('.category-content').fadeToggle("slow"); 
+      });
     });
+
+   
